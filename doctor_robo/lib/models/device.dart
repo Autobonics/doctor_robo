034ef717d981ed90fe037aae2fd4f5e008d6363d
Azhar <mohdazharsm@gmail.com>
2023-04-06 17:37:49 +0530

    Device control functionality implemented

---
+++ doctor_robo/lib/models/device.dart
@@ -0,0 +1,85 @@
+/// Device Sensor Reading model
+class DeviceReading {
+  double ph;
+  double waterFLow;
+  int totalWaterFlow;
+  int waterLevel;
+  double temp;
+  double ec;
+  DateTime lastSeen;
+
+  DeviceReading({
+    required this.ph,
+    required this.waterFLow,
+    required this.totalWaterFlow,
+    required this.waterLevel,
+    required this.temp,
+    required this.lastSeen,
+    required this.ec,
+  });
+
+  factory DeviceReading.fromMap(Map data) {
+    return DeviceReading(
+      ph: data['ph'] != null
+          ? (data['ph'] % 1 == 0 ? data['ph'] + 0.1 : data['ph'])
+          : 0.0,
+      waterFLow: data['flowRate'] != null
+          ? (data['flowRate'] % 1 == 0
+              ? data['flowRate'] + 0.1
+              : data['flowRate'])
+          : 0.0,
+      totalWaterFlow: data['totalMilliLitres'] ?? 0,
+      waterLevel: data['wtr_level'] ?? 0,
+      temp: data['temp'] != null
+          ? (data['temp'] % 1 == 0 ? data['temp'] + 0.1 : data['temp'])
+          : 0.0,
+      ec: data['ec'] != null
+          ? (data['ec'] % 1 == 0 ? data['ec'] + 0.1 : data['ec'])
+          : 0.0,
+      lastSeen: DateTime.fromMillisecondsSinceEpoch(data['ts']),
+    );
+  }
+}
+
+/// Device control model
+class DeviceData {
+  int servo;
+  int stepper;
+  bool isReadSensor;
+  bool r1;
+  bool r2;
+  bool r3;
+  bool r4;
+
+  DeviceData({
+    required this.servo,
+    required this.stepper,
+    required this.isReadSensor,
+    required this.r1,
+    required this.r2,
+    required this.r3,
+    required this.r4,
+  });
+
+  factory DeviceData.fromMap(Map data) {
+    return DeviceData(
+      servo: data['servo'] != null ? data['servo'] : 0,
+      stepper: data['stepper'] != null ? data['stepper'] : 0,
+      isReadSensor: data['isReadSensor'] ?? false,
+      r1: data['r1'] ?? false,
+      r2: data['r2'] ?? false,
+      r3: data['r3'] ?? false,
+      r4: data['r4'] ?? false,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'servo': servo,
+        'stepper': stepper,
+        'isReadSensor': isReadSensor,
+        'r1': r1,
+        'r2': r2,
+        'r3': r3,
+        'r4': r4,
+      };
+}
---
+++ doctor_robo/lib/ui/smart_widgets/online_status/online_status_viewmodel.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:doctor_robo/app/app.locator.dart';
+import 'package:doctor_robo/app/app.logger.dart';
+import 'package:doctor_robo/models/device.dart';
+import 'package:doctor_robo/services/rtdb_service.dart';
+import 'package:stacked/stacked.dart';
+
+class HomeViewModel extends BaseViewModel {
+  final log = getLogger('StatusWidget');
+
+  final _dbService = locator<RtdbService>();
+
+  DeviceReading? get node => _dbService.node;
+
+  bool _isOnline = false;
+  bool get isOnline => _isOnline;
+
+  bool isOnlineCheck(DateTime? time) {
+    if (time == null) return false;
+    final DateTime now = DateTime.now();
+    final difference = now.difference(time).inSeconds;
+    // log.i("Status $difference");
+    return difference >= 0 && difference <= 4;
+  }
+
+  late Timer timer;
+
+  void setTimer() {
+    const oneSec = Duration(seconds: 1);
+    timer = Timer.periodic(
+      oneSec,
+      (Timer timer) {
+        _isOnline = isOnlineCheck(node?.lastSeen);
+        notifyListeners();
+      },
+    );
+  }
+}
